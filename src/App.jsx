f46b0555fb4719fd541a806b91f4954b74902886
jennifer-pecky <ugwojennifer7@gmail.com>
2022-10-02 03:16:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Navbar from './component/Navbar'
+import Main from './component/Main'
+
+
+export default function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
+
+
